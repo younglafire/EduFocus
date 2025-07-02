f86d027d122d6a4ddac8f7606338f3dfd173ff90
Đoàn Gia Bảo <younglafire@gmail.com>
2025-07-02 22:43:27 +0700

    Remove Django files and focus on React frontend

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blog'
-
-urlpatterns = [
-    path('', views.blog_list, name='list'),
-    path('<int:pk>/', views.blog_detail, name='detail'),
-]
